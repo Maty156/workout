809d3ebcf2be0c469eb33fef42bfb5c70c6ed52b
Maty156 <matyasabreham7@gmail.com>
2026-05-24 11:45:27 +0300

    Update js/main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@
   'use strict';
 
   function init() {
+    // Inject Nav Icons
+    injectNavIcons();
+
     // Init nav router
     Nav.init();
 
@@ -19,6 +22,26 @@
     // Show home by default & update nav stats
     const s = Stats.compute(Store.loadTrack(), Store.getStartDate());
     Stats.updateNav(s);
+  }
+
+  function injectNavIcons() {
+    const navItems = {
+      'btn-nav-home':     'dumbbell',
+      'btn-nav-calendar': 'calendar',
+      'btn-nav-workout':  'activity',
+      'btn-nav-timer':    'timer'
+    };
+
+    for (const [id, icon] of Object.entries(navItems)) {
+      const btn = document.getElementById(id);
+      if (btn) {
+        const wrap = btn.querySelector('.nav-icon-wrap');
+        if (wrap) wrap.innerHTML = Icons.get(icon, { size: 18 });
+      }
+    }
+
+    const streakIcon = document.getElementById('nav-streak-icon');
+    if (streakIcon) streakIcon.innerHTML = Icons.get('flame', { size: 16, color: 'var(--accent2)' });
   }
 
   // Wait for DOM
