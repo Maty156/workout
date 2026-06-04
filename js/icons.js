d9f205cf68889f63038acada7b9b4290371d926f
Maty156 <matyasabreham7@gmail.com>
2026-06-04 21:26:38 +0300

    icon view updated

--- js/icons.js
+++ js/icons.js
@@ -44,3 +44,8 @@ window.Icons = {
     `.trim();
   }
 };
+
+// Additional icons added in v3
+window.Icons.paths['moon'] = '<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/>';
+window.Icons.paths['sun']  = '<circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/>';
+window.Icons.paths['user'] = '<path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/>';
